728985fbac05f1de078b6bcd203b9082dc735251
Unknown <joachim555@hotmail.com>
2019-12-02 19:58:09 +0100

    discord.io => discord.js

--- bot.js
+++ bot.js
@@ -1,42 +1,43 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
+let Discord = require('discord.js');
+let auth = require('./auth.json');
+let config = require('./config.json');
 
-// Configure logger settings
-logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
-});
-logger.level = 'debug';
-// Initialize Discord Bot
-var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+// Initialize Discord client
+let client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
+    client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
-bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
-});
+client.on('message', async message => {
+    if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: "I'm alive !"
-                });
+    let args = message.content.substring(1).split(' ');
+    let cmd = args[0];
+
+    args = args.splice(1);
+    switch (cmd) {
+        case 'ping':
+            message.reply("I'm alive !");
             break;
-            // Just add any case commands if you want to..
-         }
-     }
+        case 'loop':
+            message.channel.send("!loop");
+            break;
+        // Just add any case commands if you want to..
+    }
 });
+
+client.on("guildCreate", guild => {
+    // This event triggers when the bot joins a guild.
+    console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+    client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+client.on("guildDelete", guild => {
+    // this event triggers when the bot is removed from a guild.
+    console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+    client.user.setActivity(`Serving ${client.guilds.size} servers`);
+});
+
+client.login(auth.token);
